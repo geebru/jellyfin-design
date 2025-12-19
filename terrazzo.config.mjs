b94586fb8c30a0a2bd9e9baa692e835a0a3ae056
Greg Bruening <gregbruening@fastmail.com>
2025-12-18 20:21:52 -0500

    Remove JS output from Terrazzo

--- terrazzo.config.mjs
+++ terrazzo.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from '@terrazzo/cli';
 import css from '@terrazzo/plugin-css';
-import js from '@terrazzo/plugin-js';
 import sass from '@terrazzo/plugin-sass';
 export default defineConfig({
   tokens: ['./tokens/core/core.tokens.json'],
   plugins: [
     css(),
-    js(),
     sass()
   ],
   outDir: './dist/',
